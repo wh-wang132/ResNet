a5da95d8db2cbb5335784f5604f0d53b29c633cd
wh-wang132 <1756161456@qq.com>
2026-04-03 23:40:30 +0800

    自动qat脚本

---
+++ autorun_qat.sh
@@ -0,0 +1,48 @@
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.30_steps2_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.40_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.50_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.50_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.50_steps12_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.55_steps12_global_ft10_bs64/best_pruned_model.pth #
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.55_steps16_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.60_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet6_2d/ratio0.60_steps8_global_ft10_bs64/best_pruned_model.pth
+
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.40_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.50_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.60_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.60_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.60_steps12_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.60_steps16_global_ft10_bs64/best_pruned_model.pth #
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.65_steps12_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet10_2d/ratio0.70_steps8_global_ft10_bs64/best_pruned_model.pth
+
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.50_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.60_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.60_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.70_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.70_steps12_global_ft10_bs64/best_pruned_model.pth #
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.70_steps16_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet14_2d/ratio0.80_steps8_global_ft10_bs64/best_pruned_model.pth
+
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.50_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.60_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.60_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.70_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.80_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.80_steps12_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.80_steps16_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.85_steps16_global_ft10_bs64/best_pruned_model.pth #
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.90_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet18_2d/ratio0.90_steps12_global_ft10_bs64/best_pruned_model.pth
+
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.60_steps5_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.60_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.70_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.80_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.90_steps8_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.90_steps12_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.92_steps12_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.92_steps20_global_ft10_bs64/best_pruned_model.pth
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.94_steps20_global_ft10_bs64/best_pruned_model.pth #
+uv run src/qat_main.py --full_load True --pruning_checkpoint output/pruning/resnet34_2d/ratio0.95_steps20_global_ft10_bs64/best_pruned_model.pth
